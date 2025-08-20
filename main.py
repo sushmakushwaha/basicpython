5f2ddd7f82e3c4a437f1b04619b2ec0a888e6b06
sushmakushwaha <sushmakkushwaha20@gmail.com>
2025-08-20 10:23:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,513 @@
+#int("hello world")
+#print("sushma")
+#print("sushma is my name.")
+#print("my age is 19.")
+#print("sushma is my name.","my age is 19.")
+
+#variables
+#name = "sushma"
+#age = 19
+#price = 25.99
+#age2 = age
+
+#print(age2)
+
+#print (name)
+#print("my name is :",name)
+#print("my age is :",age)
+
+
+#print(2 ** 10)
+#print("chai" * 4)
+#score = 1000007543533
+#print(score)
+
+#name = "sushma"
+#age = 19
+#collage = "parul university"
+#price = 99.99
+#print(type(name))
+#print(type(age))
+#print(type(collage))
+#print(type(price))
+
+# a = 20
+# b = 67
+# sum = a + b
+# print(sum)
+
+
+
+# x = 1009876685
+# y = 665
+# diff = x - y
+# print(diff)
+
+
+# a = 7789
+# b = a
+# print(b)
+
+
+# age = 19
+# old = False
+# a = None
+# print(type(old))
+# print(type(age))
+# print(type(a))
+
+
+#arthmetic operators
+# a = 20
+# b = 39
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+# print(a ** b)
+
+
+#relational operators
+# a = 50
+# b = 20
+# print(a == b)
+# print(a != b)
+# print(a > b)
+# print(a < b)
+# print(a <= b)
+# print(a >= b)
+
+
+#assignment operators
+# num = 100
+# num = num + 99
+# print(num)
+
+# num = 100
+# num += 2
+# num -= 5
+# num *= 3
+# num %= 8
+# num /= 2
+# num **= 2
+# print("num :", num)
+
+
+#logical operators
+# a = 30
+# b = 20
+# print(not False)
+# print(a > b)
+# print(not(a > b))
+# val1 = False
+# val2 = False
+# print("AND operator:",val1 and val2)
+# print("OR operators:", val1 or val2)
+
+
+# print(not True)
+# print(not False)
+# print(True)
+# print(False)
+# print(True and False)
+# print(True or False)
+# print(False and True)
+
+
+#type conversion
+# a = int("20")
+# b = 3.76
+# print(a)
+# print(a + b)
+# print(type(a))
+
+# a = float("34")
+# b = 897.86
+# print(a + b)
+# print(type(a))
+
+# a = 3.14
+# a = str(a)
+# print(type(a))
+
+
+#its just think
+# a = 100
+# b = 309
+# print (a + b)
+# print(int(a))
+# print(int(a + b))
+
+
+# #  input
+# input("enter your name:")
+# name = input("enter your age:")
+# print("welcome",name)
+
+
+# val = input("enter some value:")
+# print(type(val), val)
+
+
+# int = ("7")
+# val = float(input("enter some value:"))
+# print(type(val), val)
+
+
+# name = input("enter name:")
+# age = input("enter age:")
+# marks = input("enter marks:")
+
+# print("welcome", name)
+# print("age =", age)
+# print("marks =", marks)
+
+
+# name = input("enter name:")
+# age = int(input("enter age:"))
+# marks = float(input("enter marks:"))
+
+# print("welcome", name)
+# print("age =", age)
+# print("marks =", marks)
+
+## 2 number input print sum
+# first = int(input("enter first:"))
+# second = int(input("enter second:"))
+# print("sum =", first + second)
+
+
+## WAP input side square and print area
+# side = float(input("enter side of square:"))
+# area = side ** 2
+# print("area of square =", area)
+ 
+# side = float(input("enter squar side :"))
+# print("area =", side * side)
+ 
+
+## input 2 floating point number and print their average
+# a = float(input("enter first : "))
+# b = float(input("enter second : "))
+# print("avg =", (a + b) / 2)
+
+
+##print true is greater than or equal to . not print false
+# a = 10
+# b = 5
+# print(a >= b)
+# print("not False")
+
+# a = 2
+# b = 10
+# print(a >= b)
+# print("not False")
+
+# a = int(input("enter first : "))
+# b = int(input("enter second : "))
+# print(a >= b)
+
+
+## Chapter 2
+
+#stings
+# str1  = "This is a string"
+# str2 = 'paruluniversity'
+# str3 = """This is a string""""
+
+##List
+# list = [123, "chai", 43.56]
+# print(list)
+# len(list)
+# list[-2]
+
+# str1 = "This is a string .we are creating it in python."
+# print(str1)
+# str1 = "This is a string.\nwe are creating it in python."
+# print(str1)
+# str1 = "This is a string.\twe are creating it in python."
+# print(str1)
+
+# str1 = "Parul"
+# str2 = "University"
+# print(str1 + str2)
+
+# str1 = "Parul"
+# str2 = "University"
+# final_str = (str1+str2)
+# print(final_str)
+
+# str1 = "Parul"
+# print(len(str1))
+# str2 = "University"
+# print(len(str2))
+
+# str = "paruluniversity"
+# print(len(str))
+
+# str1 = "Parul"
+# len1 = len(str1)
+# print(len1)
+# str2 = "University"
+# len2 = len(str2)
+# print(len2)
+# final_str = str1 +" " + str2
+# print(final_str)
+# print(len(final_str))
+
+
+## Indexing
+# str = "parul university"
+# char = str[0]
+# print(char)
+
+# str = "parul university"
+# print(str[3])
+# print(str[7])
+# print(str[-1])
+
+##Slicing
+str = "parul university"
+# print(str[6:])
+# print(str[:6])
+# print(str[0:6])
+# print(str[::])
+# print(str[1:3])
+# print(str[-5:-2])
+
+
+# ## String Functions
+# str = "I am studying python from parul university"
+# print(str.endswith("ity"))
+# print(str.startswith("I am"))
+# print(str.startswith("uni"))
+
+# str = "I am studying python from parul university"
+# print(str.capitalize())
+# str = str.capitalize()
+# print(str)
+
+# str = "I am studying python from parul university"
+# print(str.replace("parul", "ahmedabad"))
+# print(str.replace("a","o"))
+# print(str.replace("python", "javascript"))
+
+# str = "I am studying python from parul university"
+# print(str.find("parul"))
+# print(str.find("o"))
+# print(str.find("q"))
+
+# str = "I am studying python from parul university"
+# print(str.count("u"))
+# print(str.count("p"))
+
+# str = "I am studying python from parul university"
+# print(str.upper())
+# print(str.lower())
+# print(str.title())
+# print(str.capitalize())
+# print(str.count("u"))
+# print(str.find("parul"))
+# print(str.replace("parul", "ahmedabad"))
+# print(str.split(" "))
+# print(str.split(",  "))
+# print(str.endswith("ity"))
+# print(str.startswith("I am"))
+# print(str.startswith("uni"))
+
+
+# name = "sushma kumari kushwaha"
+# print(name)
+# print(len(name))
+# print(name[8])
+# print(name[0:5])
+
+##Input user first name & print length.
+# name = input("enter your name : ")
+# print("length of your name is", len(name))
+
+## find occurrence of '$' in string
+# str = "Hi, $Iam the $ symbol $99.99"
+# print(str.count("$"))
+
+## Conditional Statements
+# age = 20
+# if(age > 18):
+#     print("can vote")
+#     print("can drive car")
+
+# age = 20 
+# if (True):
+#     print("can vote")
+#     print("can drive car")
+
+# light = "pink"
+# if(light =="red"):
+#     print("stop")
+# elif(light == "green"):
+#     print("go")
+# elif(light == "yellow"):
+#     print("look")  
+# else:
+#     print("light is broken")  
+# print("end of code")
+
+# num = 5
+# if(num>2):
+#     print("greater than 2")
+# if(num>3):
+#     print("greater than 3")    
+
+# age = 19
+# if(age>=18):
+#     print("can vote") 
+# else:
+#     print("cannot vote")    
+
+# marks  = 75
+# if(marks >= 90):
+#     grade = "A"
+# elif(marks >= 80 and marks < 90):
+#     grade =  "B"
+# elif(marks >= 70 and marks < 80):
+#     grade = "C"
+# else:
+#     grade = "D"
+
+# print("grade of the student ->", grade)
+
+# marks  = int(input("enter student marks : "))
+# if(marks >= 90):
+#     grade = "A"
+# elif(marks >= 80 and marks < 90):
+#     grade =  "B"
+# elif(marks >= 70 and marks < 80):
+#     grade = "C"
+# else:
+#     grade = "D"
+# print("grade of the student ->", grade)
+
+
+##Nesting
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
